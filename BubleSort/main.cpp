cc82f556aea5c6e599ba9cffea3c2d75f99e4481
Eduardo555 <eduardospillereanzolin@gmail.com>
2016-11-13 19:00:47 -0200

    Adicionada pacote de classes para algoritimo de buble sort

---
+++ BubleSort/main.cpp
@@ -0,0 +1,35 @@
+/*
+ *DESENVOLVIMENTO DO ALGORITIMO QUE CALCULA O FATORIAL DE UM NUMERO
+ * Academicos: Eduardo Spillere Anzolin e Victor Pavei Goes;
+ * Data: 13/11/2016
+ * Universidade do extremo sul catarinense;
+*/
+
+#include <iostream>
+#include "func_fatorial.h"
+
+
+using namespace std;
+
+func_bubleSort f;
+
+void menu(){
+    cout<<"======MENU======="<<endl;
+    cout<<"0-Sair"<<endl;
+    cout<<"1-Entrada do teclado:"<<endl;
+    cout<<"2-Entrada aleatoria:"<<endl;
+}
+
+int main(){
+
+    int op = -1;
+    cin>>op;
+    while(op != 0){
+        switch(op){
+            case 1:
+
+        }
+    }
+
+}
+
