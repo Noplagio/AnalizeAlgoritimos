1905e2d0cd44f841589b056b4f3effd601d375f9
Victor cagão <victor.pavei.goes@gmail.com>
2016-11-15 19:54:35 -0200

    Quick pronto

---
+++ QuickSort/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+#define TAM 10
+
+void quick(int vet[], int esq, int dir)
+{
+
+    int pivo = esq, i,ch,j;
+
+for(i=esq+1;i<=dir;i++)
+    {
+
+    j = i;
+
+    if(vet[j] < vet[pivo])
+        {
+        ch = vet[j];
+
+        while(j > pivo)
+            {
+            vet[j] = vet[j-1];
+            j--;
+            }
+
+        vet[j] = ch;
+        pivo++;
+        }
+
+    }
+    if(pivo-1 >= esq)
+        {
+        quick(vet,esq,pivo-1);
+    }
+    if(pivo+1 <= dir)
+        {
+        quick(vet,pivo+1,dir);
+    }
+ }
+
+int main()
+{
+
+    int vet[TAM],i;
+
+    cout <<"Digite 10 numeros a sua escolha(a cada numero de ENTER e digite outro)" << endl;
+
+    for(i=0;i<TAM;i++)
+
+        cin >> vet[i];
+
+    quick(vet,0,TAM-1);
+
+    cout << "" << endl;
+    cout << "Valores ordenados pelo QuickSort" << endl;
+    cout << "" << endl;
+    for(i=0;i<TAM;i++)
+
+        cout << vet[i] << endl;
+
+    return 0;
+}
