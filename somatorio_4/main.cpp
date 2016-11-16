470eb23a414df3066667e61fabed9ae40e809f90
Eduardo555 <eduardospillereanzolin@gmail.com>
2016-11-15 22:38:01 -0200

    Finalizada função do item 4

--- somatorio_4/main.cpp
+++ somatorio_4/main.cpp
@@ -2,8 +2,24 @@
 
 using namespace std;
 
+void cria_sequencia(int n){
+    int j=1;
+    cout<<"\n"<<endl;
+    for(int i=1;i<n;i++){
+        cout<<i<<i+3<<i+3;
+    }
+    cout<<"\n"<<endl;
+}
+
+
 int main(){
 
+    int n;
+    cout<<"Informe o tamanho da sequencia:"<<endl;
+    cin>>n;
 
+    cout<<"Criando sequencia!!"<<endl;
+
+    cria_sequencia(n);
 
 }
